6ece590724e9e9d9655201025c8bfe1ea061b754
马遥 <mervin1024@163.com>
2016-11-11 16:00:37 +0800

    Initial Commit

---
+++ DKPrecord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DKPrecord
+//
+//  Created by mervin on 2016/11/11.
+//  Copyright © 2016年 mervin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
